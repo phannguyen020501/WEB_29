36ca7bbbbadc32f59d99291c766cf2318e230809
phannguyen020501 <phannguyen147852@gmail.com>
2022-06-08 09:51:44 +0700

    e-com

---
+++ Lab5/e-commerce/application/views/header.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>E-Commerce</title>
+<style>
+
+
+:focus {
+	outline: 0;
+}
+body {
+	line-height: 1;
+	color: black;
+	background: white;
+	
+}
+ol, ul {
+	list-style: none;
+}
+
+table {
+	border-collapse: separate;
+	border-spacing: 0;
+}
+caption, th, td {
+	text-align: left;
+	font-weight: normal;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+	content: "";
+}
+blockquote, q {
+	quotes: "" "";
+}
+html { overflow: -moz-scrollbars-vertical; }
+html { overflow-x: auto; }
+
+
+h1 {
+color:red;
+font-family: Cambria, serif;
+font-size: 50px;
+font-style: normal;
+font-weight: bold;
+text-transform: uppercase;
+letter-spacing: 1px;
+line-height: 1.7em;
+padding:0;
+margin:0;
+padding:10px;
+}
+
+h2 {
+color:blue;
+font-family: Cambria, serif;
+font-size: 30px;
+font-style: normal;
+font-weight: bold;
+text-transform: uppercase;
+letter-spacing: 1px;
+line-height: 1.7em;
+padding:0;
+margin:0;
+padding:10px;
+}
+
+.category {
+font-family: Georgia, "Times New Roman", Times, serif;
+margin:10px;
+margin-left:0px;
+margin-bottom:20px;
+}
+
+.tag {
+font-family: Georgia, "Times New Roman", Times, serif;
+
+padding:10px;
+}
+
+.category a {
+font-size:14px;
+padding:10px;
+color:#B33333;
+text-decoration:none;
+}
+
+.tag{
+	color: blue;
+	font-size : 20px;
+}
+
+
+.category a:hover{ 
+background-color:#FAFF33
+}
+	</style>
+</head>
+
+<body>
+<div class="navigation"><h1>E-Commerce</h1>
---
+++ Lab5/e-commerce/config/config.php
@@ -0,0 +1,16 @@
+<?php
+
+/** Configuration Variables **/
+
+define ('DEVELOPMENT_ENVIRONMENT',true);
+
+
+define('DB_NAME', 'ecom');
+define('DB_USER', 'root');
+define('DB_PASSWORD','');
+define('DB_HOST', 'localhost:3308');
+
+define('BASE_PATH','http://localhost/web29/lab5/e-commerce/');
+
+
+define('PAGINATE_LIMIT', '5');
