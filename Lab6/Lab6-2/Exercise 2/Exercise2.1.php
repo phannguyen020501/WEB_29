52a629e0be2b845bd988d4194d3a301791febfb3
linhvu2001 <88344263+linhvu2001@users.noreply.github.com>
2022-06-14 23:18:18 +0700

    Create Exercise2.1.php

---
+++ Lab6/Lab6-2/Exercise 2/Exercise2.1.php
@@ -0,0 +1,44 @@
+<html>
+    <head>
+        <title>THREE-COLUMN LIQUID LAYOUT WITH FLOATING BOXES</title>
+        <style type="text/css">
+        <!--
+        body {
+        margin: 0px;
+        padding: 0px;
+        }
+        #header {
+        background: red;
+        width: 100%;
+        }
+        #rightcol {
+        background: yellow;
+        float: right;
+        width: 20%;
+        height: 500px;
+        }
+        #content {
+        background: #fff;
+        float: left;
+        width: 79%;
+        height: 500px;
+        }
+        #footer {
+        background: blue;
+        clear: both;
+        width: 100%;
+        color: white;
+        }
+        -->
+        </style>
+    </head>
+    <body>
+        <div id="header">Header content</div>
+        <div id="content">
+        <p style="font-size: 48px">Main content</p>
+        </div>
+        <div id="rightcol">
+            <p>Sidebar content</p>
+        </div>
+        <div id="footer">Footer Section</div></body>
+</html>
